6c67f48ac51fb367e8ecf120561c8e49fb24df90
RicardoOliveira-5 <a49436@alunos.isel.pt>
2026-03-27 14:47:00 +0000

    again

--- src/main/kotlin/pt/unl/fct/iadi/bookstore/security/SecurityConfig.kt
+++ src/main/kotlin/pt/unl/fct/iadi/bookstore/security/SecurityConfig.kt
@@ -57,6 +57,7 @@ class SecurityConfig {
                 .requestMatchers(SWAGGER_UI, V3_API_DOCS).permitAll()
                 .requestMatchers("/v3/api-docs", SWAGGER_UI, V3_API_DOCS).permitAll()
                 .requestMatchers("/v3/api-docs", "/v3/api-docs/**", SWAGGER_UI).permitAll()
+                .requestMatchers("/v3/api-docs/**", "/v3/api-docs", "/swagger-ui/**", "/swagger-ui.html").permitAll()
 
 
                 // GET requests → qualquer utilizador autenticado

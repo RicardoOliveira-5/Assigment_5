5e10a6ef36fd2b534e3d92f3c2bf981381fc7a18
RicardoOliveira-5 <a49436@alunos.isel.pt>
2026-03-27 23:10:01 +0000

    night life

--- src/main/kotlin/pt/unl/fct/iadi/bookstore/service/BookNotFoundException.kt
+++ src/main/kotlin/pt/unl/fct/iadi/bookstore/service/BookNotFoundException.kt
@@ -1,3 +1,7 @@
 package pt.unl.fct.iadi.bookstore.service
 
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.ResponseStatus
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
 class BookNotFoundException(message: String) : RuntimeException(message)
--- src/main/kotlin/pt/unl/fct/iadi/bookstore/service/ReviewNotFoundException.kt
+++ src/main/kotlin/pt/unl/fct/iadi/bookstore/service/ReviewNotFoundException.kt
@@ -1,3 +1,8 @@
 package pt.unl.fct.iadi.bookstore.service
 
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.ResponseStatus
+
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
 class ReviewNotFoundException(reviewId: Long) : RuntimeException("Review with ID $reviewId not found")
